1bac9926e0420404303f9b4fef55fc39240d624d
Tommy Giardina <tommy.giardina@gmail.com>
2020-07-28 18:02:12 -0500

    Impl Result
    Impl a result class so that I can give descriptive http error messages.
    
    Signed-off-by: Tommy Giardina <tommy.giardina@gmail.com>

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -6,6 +6,13 @@ import { createUser } from '../services';
 export default function init(server: Application): void {
   server.post("/users", async (req, res) => {
     const username = req.body.username;
-    res.json(await createUser(username));
+    const result = await createUser(username);
+    if(result.isSuccess) {
+      res.json(result.getValue());
+    } else if(result.error == "ER_DUP_ENTRY") {
+      res.status(409).json(`409 - User "${username}" already exists`);      
+    } else {
+      res.status(500).json("500 - Server error");      
+    }
   });
 }
--- src/services/create-user.ts
+++ src/services/create-user.ts
@@ -1,16 +1,17 @@
 import { getConnection } from'typeorm';
 
-import { User } from '../models';
+import { User }   from '../models';
+import { Result } from '../helpers';
 
-export async function createUser(username: string): Promise<User> {
+export async function createUser(username: string): Promise<Result<User>> {
   try {
     const user     = new User();
     user.joined    = new Date();
     user.lastLogin = user.joined;
     user.username  = username;
     await getConnection().manager.save(user);
-    return user;
+    return Result.ok(user);
   } catch(err) {
-    console.error(err);
+    return Result.fail(err.code);
   }
 }
