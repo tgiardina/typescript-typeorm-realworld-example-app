c96cbd1b0fdc82a250714f0dd4ee2015b5173c16
Tommy Giardina <tommy.giardina@gmail.com>
2020-08-06 13:35:53 -0500

    Refactored container
    Now loads service as singleton.
    
    Signed-off-by: Tommy Giardina <tommy.giardina@gmail.com>

--- src/loaders/container.ts
+++ src/loaders/container.ts
@@ -9,9 +9,9 @@ import { UserService, IUserService } from '../services';
 
 export function loadContainer(): Container {
   const container = new Container();
-  container.bind<IUserRepository>(TYPES.UserRepository).toDynamicValue(() => {
-    return getRepository(UserModel);
-  });
+  container
+    .bind<IUserRepository>(TYPES.UserRepository)
+    .toConstantValue(getRepository(UserModel));
   container.bind<IUserService>(TYPES.UserService).to(UserService);
   return container;
 }
