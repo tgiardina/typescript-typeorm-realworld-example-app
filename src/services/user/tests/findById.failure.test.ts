8cb01c5d62013eed8ff5e1af6cc07fbd19592019
Tommy Giardina <tommy.giardina@gmail.com>
2020-08-12 13:18:29 -0500

    Added findById tests
    Signed-off-by: Tommy Giardina <tommy.giardina@gmail.com>

---
+++ src/services/user/tests/findById.failure.test.ts
@@ -0,0 +1,55 @@
+import 'reflect-metadata';
+import { assert } from 'chai';
+import 'mocha';
+import { createSandbox, SinonSandbox, SinonStub } from 'sinon';
+
+import { UserService } from '../';
+import { Result } from '../../../helpers';
+import { IUserDto } from '../../../models';
+
+describe('UserService.findById', () => {
+  const data = {
+    id: 1,
+    username: "username",
+    token: "token",
+  };
+  let result: Result<IUserDto>;
+  let sandbox: SinonSandbox;
+  let userService: UserService;
+
+  before(async () => {
+    sandbox = createSandbox();
+    const userModel = {
+      ...data,
+      toDto: () => {
+        return {
+          ...data,
+        };
+      }
+    };
+    const userRepository = {
+      create: sandbox.stub(),
+      findOne: sandbox.stub(),
+      save: sandbox.stub(),
+    };
+    userService = new UserService(userRepository);
+  });
+
+  after(async () => {
+    sandbox.restore();
+  });
+
+  describe('is passed an invalid id', () => {
+    it('should run without error', async () => {
+      result = await userService.findById(data.id);
+    })
+
+    it('should return an ok result', async () => {
+      assert(!result.isOk);
+    });
+
+    it('should return the correct error code', async () => {
+      assert.equal(result.error, "ER_NOT_FOUND");
+    });
+  });
+}) 
